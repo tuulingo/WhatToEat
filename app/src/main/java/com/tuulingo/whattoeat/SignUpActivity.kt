0f04fc9748d6574cbce5f599fd72e44a85b46fec
tuulingo <oliver.tuuling@tptlive.ee>
2022-08-02 14:45:59 +0300

    Add Firebase and functioning login / signup

--- app/src/main/java/com/tuulingo/whattoeat/SignUpActivity.kt
+++ app/src/main/java/com/tuulingo/whattoeat/SignUpActivity.kt
@@ -1,11 +1,80 @@
 package com.tuulingo.whattoeat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.tuulingo.whattoeat.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        auth = Firebase.auth
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGotoLogin.setOnClickListener { switchToLogInActivity() }
+
+        binding.btnSignup.setOnClickListener {
+            val email = binding.etSignupEmail.text.toString()
+            val password = binding.etSignupPassword.text.toString()
+            val confirmPassword = binding.etSignupConfirmpassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
+                if (password == confirmPassword){
+                    createAccount(email, password)
+                }else{
+                    Toast.makeText(this, "Passwords are not matching", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Fill all the fields", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
+
+    private fun switchToLogInActivity() {
+        val switchActivityIntent = Intent(this, LogInActivity::class.java)
+        startActivity(switchActivityIntent)
+    }
+
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "createUserWithEmail:success")
+                    switchToLogInActivity()
+/*                    val user = auth.currentUser
+                    updateUI(user)*/
+                } else {
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
+
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
+
+
+
 }
